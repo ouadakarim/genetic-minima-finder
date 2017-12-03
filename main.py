55818965086b14a32ab9ba0f71a4570ed9d89710
Lisu <michaell@10g.pl>
2017-12-03 21:40:05 +0100

    Possibility to specify evaluation range added

--- main.py
+++ main.py
@@ -5,12 +5,28 @@ from genetics import Algorithm
 
 POLYNOMIAL_REGEX=r"(^-)?(((\d+(\.\d+)?)?(x(\^\d)?)?)([+-](?!$))?)*"
 
+def int_input(msg):
+	inp = input(msg)
+	if not inp:
+		return None
+	try:
+		return int(inp)
+	except:
+		print("Not an integer")
+		sys.exit(-1)
+
 print("Provide polynomial to analyze")
 print("Example: -1.23x^4")
-user_input = input()
+user_input = input(">> ")
 
 if not re.fullmatch(POLYNOMIAL_REGEX, user_input):
 	print("Provided function is not valid polynomial of variable x")
+	sys.exit(-1)
+
+min_x = int_input("Provide start of evalution range (min x, default -100): ")
+max_x = int_input("Provide end of evalution range (max x, default 100): ")
+if min_x >= max_x:
+	print("Min x has to be smaller than max x")
 	sys.exit(-1)
 
 user_input = "lambda x: " + user_input.replace("^", "**")
@@ -21,6 +37,6 @@ function = eval(function_st)
 
 algorithm = Algorithm(function)
 # print(algorithm.calculate())
-print("Local minimums at:", algorithm.find_local_minimums())
+print("Local minimums at:", algorithm.find_local_minimums(min_val=min_x, max_val=max_x))
 
 
